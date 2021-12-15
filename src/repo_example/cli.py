0a76d4d66afa54eaa563a47b5711d15c4c634f1a
Felix Ball <Felix.Ball@ovgu.de>
2021-12-15 17:25:33 +0100

    fuck my life ...

--- src/repo_example/cli.py
+++ src/repo_example/cli.py
@@ -7,11 +7,15 @@ import click
 @click.command()
 def main(args=None):
     """Console script for repo_example."""
-    click.echo("Replace this message by putting your code into "
-               "repo_example.cli.main")
+    click.echo(
+        "Replace this message by putting your code into " "repo_example.cli.main"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
+
+    # sdafrasdgfysf
+# adf aasd
